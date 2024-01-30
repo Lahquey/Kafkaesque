af19157cf8d38519ce72a201638ad1ac2bbe7c4e
Lahquey <luckygriffin2@gmail.com>
2024-01-30 09:31:46 -0800

    Incomplete work, to be continued

---
+++ Average.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+	int arr[];
+	int size;
+	if (size<=0){
+		printf("Error, arra must be greater than 0. \n");
+	}
+
+	int Some = 0;
+	for (int i =0; i<size; ++i){
+		Some += arr[i];
+	}
+
+	int ArrayElements[] = {4,56,85245,85,2};
+
+	double Avg = Some/
